03529f865c86a5b6dcde42d7b8b739e2950e2f7d
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2024-02-08 13:26:57 -0400

    chequeando eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
+  }
+}
